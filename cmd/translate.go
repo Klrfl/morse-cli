c90c8cd3b293a7cd53d9f82f1f0b6815c70ec438
Efraim Munthe <efrayanglain@gmail.com>
2024-03-12 10:37:49 +0700

    add translate command

--- cmd/translate.go
+++ cmd/translate.go
@@ -1,11 +1,12 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 NAME HERE <efrayanglain@gmail.com>
 
 */
 package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -23,6 +24,63 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("translate called")
 	},
+}
+
+func translator(word string) string {
+	if len(word) <= 0 {
+		return ""
+	}
+
+	var result strings.Builder
+
+	for _, char := range strings.ToUpper(word) {
+		varc := string(char)
+		result.WriteString(translationTable[varc])
+		result.WriteString(" ")
+	}
+
+	return result.String()
+}
+
+var translationTable map[string]string = map[string]string{
+	"A": ".-",
+	"B": "-...",
+	"C": "-.-.",
+	"D": "-..",
+	"E": ".",
+	"F": "..-.",
+	"G": "--.",
+	"H": "....",
+	"I": "..",
+	"J": ".----",
+	"K": "-.-",
+	"L": ".-..",
+	"M": "--",
+	"N": "-.",
+	"O": "---",
+	"P": ".--.",
+	"Q": "--.-",
+	"R": ".-.",
+	"S": "...",
+	"T": "-",
+	"U": "..-",
+	"V": "...-",
+	"W": ".--",
+	"X": "-..-",
+	"Y": "-.--",
+	"Z": "--..",
+	" ": "/",
+
+	"0": "-----",
+	"1": ".----",
+	"2": "..---",
+	"3": "...--",
+	"4": "....-",
+	"5": ".....",
+	"6": "-....",
+	"7": "--...",
+	"8": "---..",
+	"9": "----.",
 }
 
 func init() {
