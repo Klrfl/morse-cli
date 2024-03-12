83f334feca8ca17e3ecdab095151429687954904
Efraim Munthe <efrayanglain@gmail.com>
2024-03-12 10:32:43 +0700

    init cobr

--- main.go
+++ main.go
@@ -1,68 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <efrayanglain@gmail.com>
+
+*/
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-var translationTable map[string]string = map[string]string{
-	"A": ".-",
-	"B": "-...",
-	"C": "-.-.",
-	"D": "-..",
-	"E": ".",
-	"F": "..-.",
-	"G": "--.",
-	"H": "....",
-	"I": "..",
-	"J": ".----",
-	"K": "-.-",
-	"L": ".-..",
-	"M": "--",
-	"N": "-.",
-	"O": "---",
-	"P": ".--.",
-	"Q": "--.-",
-	"R": ".-.",
-	"S": "...",
-	"T": "-",
-	"U": "..-",
-	"V": "...-",
-	"W": ".--",
-	"X": "-..-",
-	"Y": "-.--",
-	"Z": "--..",
-	" ": "/",
-
-	"0": "-----",
-	"1": ".----",
-	"2": "..---",
-	"3": "...--",
-	"4": "....-",
-	"5": ".....",
-	"6": "-....",
-	"7": "--...",
-	"8": "---..",
-	"9": "----.",
-}
-
-func translator(word string) string {
-	if len(word) <= 0 {
-		return ""
-	}
-
-	var result strings.Builder
-
-	for _, char := range strings.ToUpper(word) {
-		varc := string(char)
-		result.WriteString(translationTable[varc])
-		result.WriteString(" ")
-	}
-
-	return result.String()
-}
+import "github.com/klrfl/morse-translator/cmd"
 
 func main() {
-	fmt.Println(translator("we can translate whole sentences now"))
-	fmt.Println(translator(""))
+	cmd.Execute()
 }
